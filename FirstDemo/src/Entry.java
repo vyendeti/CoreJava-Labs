666651c97954a643148f294fda1cc9dc28665d45
abhishek-ghode <abhishek.ghode@capgemini.com>
2018-04-05 11:31:26 +0530

    Adding a constructor concept in a project

--- FirstDemo/src/Entry.java
+++ FirstDemo/src/Entry.java
@@ -1,7 +1,29 @@
 import static java.lang.System.out;
 
 class Entry{
+	
+	
 	public static void main(String[] args){
-		out.print("Hello, world!");
+		out.println("Hello, world!");
+		
+		Customer c = null;
+		
+		c = new Customer();
+		c.displayDetails();
+
+		c = new Customer("RAJ",412101);
+		c.displayDetails();
+		
+		c.getInvoices();
+		
 	}
+	
+	
 }
+
+
+
+
+
+
+
